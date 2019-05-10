6605f58cfc5260d08416d4aef8828e66b5ef22d2
Rongcui Dong <rongcuid@outlook.com>
2019-05-10 15:21:30 -0400

    Basic spec

--- src/main/scala/riscv/stagezero/TopLevel.scala
+++ src/main/scala/riscv/stagezero/TopLevel.scala
@@ -1,7 +1,21 @@
 package riscv.stagezero
 
-import spinal.core.Component
+import spinal.core._
 
-class TopLevel extends Component {
+class TopLevel(firmware: Array[Data]) extends Component {
+  val io: Bundle = new Bundle {
+    val mem_ready = in Bool
+    val mem_valid = out Bool
+    val mem_addr  = out UInt(32 bits)
+    val mem_rdata = in Bits(32 bits)
+    val mem_wstrb = out Bits(4 bits)
+    val mem_wdata = out Bits(32 bits)
+  }
+}
 
+object StageZeroTopLevel {
+  def main(args: Array[String]): Unit = {
+    SpinalVerilog(new TopLevel(new Array[Data](0)))
+    SpinalVhdl(new TopLevel(new Array[Data](0)))
+  }
 }
