b47bb36071dc17ec1920e4ba7f8c338ba6711550
Rongcui Dong <rongcuid@outlook.com>
2019-05-11 17:27:47 -0400

    ALU端口重构

--- src/main/scala/riscv/stagezero/core/SZAlu.scala
+++ src/main/scala/riscv/stagezero/core/SZAlu.scala
@@ -2,20 +2,25 @@ package riscv.stagezero.core
 
 import spinal.core._
 
+/**
+  * ALU操作枚举
+  */
 object SZAluOp extends SpinalEnum {
   val Add, Sub, Sll, Slt, Xor, Srl, Sra, Or, And = newElement()
+}
+
+case class SZAluIO() extends Bundle {
+  val op1: Bits = in Bits(32 bits)
+  val op2: Bits = in Bits(32 bits)
+  val signed: Bool = in Bool
+  val opSel: SpinalEnumCraft[SZAluOp.type] = in(SZAluOp)
+  val res: Bits = out Bits(32 bits)
 }
 
 /**
   * 运算逻辑单元（ALU）
   */
 case class SZAlu() extends Component {
-  val io = new Bundle {
-    val op1: Bits = in Bits(32 bits)
-    val op2: Bits = in Bits(32 bits)
-    val signed: Bool = in Bool
-    val opSel: SpinalEnumCraft[SZAluOp.type] = in(SZAluOp)
-    val res: Bits = out Bits(32 bits)
-  }
+  val io = SZAluIO()
   // TODO
 }
